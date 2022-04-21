d7232733717b6beebfb1368bf2bc515638124e92
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-04-21 11:40:15 -0600

    added concurrently to package json start script. Almost able to call api from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [question, setQuestion] = useState('')
+  const [answer, setAnswer] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const submitForm = async () => {
+    setLoading(true)
+    const res = await fetch('/prompt', { method: 'post', body: JSON.stringify({ prompt: question }) })
+    if (res.ok) {
+      const answer = await await res.json()
+      setAnswer(answer)
+      setLoading(false)
+    }
+    setLoading(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CyberDuck</h1>
+        <p>Your AI Powered Rubber Ducky Debugging Platform</p>
       </header>
+
+      {loading ? (
+        <p>Loading... </p>
+      ) : (
+        <div>
+          <div>
+            <form className="form" onSubmit={submitForm}>
+              <label>Describe your dilema: </label>
+              <textarea value={question} onChange={e => setQuestion(e.value)}></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+
+          {answer ? <div className="answer">{answer}</div> : null}
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
